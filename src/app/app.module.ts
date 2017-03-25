472fce6f15f6ebed45bdabd8a7c1a3ebf387473d
Logan Livingston <loganlivingston@logans-mbp.attlocal.net>
2017-03-24 20:02:08 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ServicesComponent } from './services/services.component';
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+
+const appRoutes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'account',  component: AccountComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ServicesComponent,
+    HomeComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
